76c8b3469d42e031a77627105701909846313186
Ubuntu <ubuntu@i-0cb0cd6fc18de3139.ec2.internal>
2023-02-20 17:40:12 +0000

    fit: cambio de nombres a minusculas

--- src/controllers/flights-details.ctrl.js
+++ src/controllers/flights-details.ctrl.js
@@ -43,8 +43,8 @@ const createUpdateFlightDetails = async (req, res) => {
       minutes_logbook: minutesLogbook,
       send_email: sendEmail,
       zync_uploaded_timestamp: zyncUploadedTimestamp,
-      Route_From_Sub: RouteFromSub,
-      Route_To_Sub: RouteToSub,
+      route_from_sub: RouteFromSub,
+      route_to_sub: RouteToSub,
     } = req.body;
 
     const findUUID = await prisma.tbl_rp_flight_details.findFirst({
@@ -111,8 +111,8 @@ const createUpdateFlightDetails = async (req, res) => {
       zync_uploaded_timestamp: zyncUploadedTimestamp
         ? dateFormat(zyncUploadedTimestamp)
         : null,
-      Route_From_Sub: RouteFromSub,
-      Route_To_Sub: RouteToSub,
+      route_from_sub: RouteFromSub,
+      route_to_sub: RouteToSub,
     };
 
     const filterDataFlightsDetail = await formatData(dataFlightsDetail);
